a8300ac2afc23cf4fe2942574ae0f5ccdb1859d7
Mihara3389 <m_mihara@emyus.jp>
2021-05-17 17:01:30 +0900

    top画面へ遷移するように遷移先を変更

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('auth/top', [App\Http\Controllers\Auth\TopController::class, 'index'])->name('top');
