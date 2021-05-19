312afcd146d87688dba8b6c7c1d38de1d6306dc3
Mihara3389 <m_mihara@emyus.jp>
2021-05-19 21:16:38 +0900

    updated_atの自動挿入を無効にする

--- app/Models/History.php
+++ app/Models/History.php
@@ -9,6 +9,9 @@ class History extends Model
 {
 	use HasFactory;
 
+	//自動挿入されるupdated_atを無効にする
+	const UPDATED_AT = NULL;
+
 	protected $table = "histories";
 	
 	protected $fillable = ['id', 'user_id', 'point', 'created_at'];
