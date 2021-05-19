8cb3e332467d60de7cde81b182b564bac0345acd
Mihara3389 <m_mihara@emyus.jp>
2021-05-19 10:24:04 +0900

    問題と答えのテーブル追加

---
+++ database/migrations/2021_05_19_011103_create_questions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('question',511);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
