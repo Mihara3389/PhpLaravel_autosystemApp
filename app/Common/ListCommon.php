3fc03660f2828ccadd07fde7dbe78bd2873e5e66
Mihara3389 <m_mihara@emyus.jp>
2021-05-21 11:56:44 +0900

    配置修正

--- app/Common/ListCommon.php
+++ app/Common/ListCommon.php
@@ -16,6 +16,6 @@ class ListCommon
         //questions.id毎に連番付与
         $lists = DB::select('SELECT questions.id as id, questions.question , correct_answers.id as answer_id ,correct_answers.answer as answer,if(@groupid <>  questions.id, @no:=1, @no:=@no+1) as no,@groupid:= questions.id  
         FROM questions LEFT JOIN correct_answers ON questions.id = correct_answers.question_id'); 
-         return $lists;
+        return $lists;
     }
 }
