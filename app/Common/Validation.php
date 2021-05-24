8c6260f7ba2cf511c25ed0107b34f7fa5fb188d6
Mihara3389 <m_mihara@emyus.jp>
2021-05-24 13:39:35 +0900

    新規登録の重複チェック

--- app/Common/Validation.php
+++ app/Common/Validation.php
@@ -10,7 +10,7 @@ class Validation
     public function rules(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'question' => 'required|string|max:511',
+            'question' => 'required|string|max:511|unique:questions,question',
             'question.0' => 'required|string|max:511',
             'answer.0' => 'required|string|max:255',
             'answer.*' => 'max:255',
--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -118,7 +118,7 @@ return [
     'starts_with' => 'The :attribute must start with one of the following: :values.',
     'string' => 'The :attribute must be a string.',
     'timezone' => 'The :attribute must be a valid zone.',
-    'unique' => 'ユーザー名はすでに登録されています。',
+    'unique' => '登録済です。',
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute format is invalid.',
     'uuid' => 'The :attribute must be a valid UUID.',
